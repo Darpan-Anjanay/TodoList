b4beb695bc9092ccd013c9c63db0e0b230f7eb08
Darpan-Anjanay <darpangurjar28@gmail.com>
2025-08-06 13:45:48 +0530

    Update

--- TodoApp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Todo
-
-class TodoSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['id','user','Title','CompletionDate','completionStatus','IsDelete']
-        read_only_fields = ['id', 'created_at']
-
